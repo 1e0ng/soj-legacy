89927b8bceea7624b36aa443b071fb8877af1a84
Liang Sun <i@liangsun.org>
2010-01-19 02:48:42 +0000

    git-svn-id: https://soj.googlecode.com/svn/trunk@77 6050238e-8759-11de-82e5-a1837a67dc72

--- src/web/bottom.php
+++ src/web/bottom.php
@@ -7,7 +7,7 @@ else
 <div align = "bottom">
 <font face="sans-serif">
 	<hr width = "80%">
-	<center>All Rights Reserved 2009-2009.</center>
+	<center>All Rights Reserved 2009-2010.</center>
 	<center>
 	<a href=mailto:powerx1202@gmail.com>powerx1202</a>
 	&nbsp;&nbsp;
