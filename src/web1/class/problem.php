986095d22f435b89c60e9e063087b7ee90a7b964
powerx1202 <powerx1202@6050238e-8759-11de-82e5-a1837a67dc72>
2010-05-03 14:35:07 +0000

    add inifile
    git-svn-id: https://soj.googlecode.com/svn/trunk@92 6050238e-8759-11de-82e5-a1837a67dc72

---
+++ src/web1/class/problem.php
@@ -0,0 +1,16 @@
+<?php
+class Problem
+{
+    private $pid;
+    private $problemName;
+    private $accepted;
+    private $submitted;
+    private $discription;
+    private $input;
+    private $output;
+    private $inputCase;
+    private $outputCase;
+    private $timeLimit;
+    private $memoryLimit;
+}
+?>
---
+++ src/web1/class/status.php
@@ -0,0 +1,14 @@
+<?php
+class Status
+{
+    private $rid;
+    private $pid;
+    private $uid;
+    private $judgeStatus;
+    private $language;
+    private $rtime;
+    private $rmemory;
+    private $submitTime;
+    private $sourceCode;
+}
+?>
