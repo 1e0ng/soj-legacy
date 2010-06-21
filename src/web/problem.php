4cbe5029d55958a4c3f7e4896aedfc0e65369066
Liang Sun <i@liangsun.org>
2010-06-21 05:58:07 +0000

    fix images
    git-svn-id: https://soj.googlecode.com/svn/trunk@168 6050238e-8759-11de-82e5-a1837a67dc72

--- src/web/problem.php
+++ src/web/problem.php
@@ -69,8 +69,8 @@ gen_section("OUTPUT", my_htmlentities($problem->output));
 gen_section("INPUT CASE", my_htmlentities($problem->inputCase));
 gen_section("OUTPUT CASE", my_htmlentities($problem->outputCase));
  */
-include("problem/{$problem->pid}.htm");
-
+$infile="problem/{$problem->pid}.php";
+include $infile;
 echo "<div align = \"center\">\n";
 echo "<a href=\"submitproblem.php?pid=$pid\">Submit</a>";
 echo "&nbsp;&nbsp;&nbsp;&nbsp;";
