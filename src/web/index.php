1227aadf473133bcc52b047497e458eb8dfac013
Liang Sun <i@liangsun.org>
2012-09-29 02:53:49 +0000

    Add google analytics.
    git-svn-id: https://soj.googlecode.com/svn/trunk@308 6050238e-8759-11de-82e5-a1837a67dc72

--- src/web/index.php
+++ src/web/index.php
@@ -14,6 +14,19 @@ if($DEBUG)
 <link rel="stylesheet" href="static/css.css" type="text/css" />
 <script type="text/javascript" src="static/jquery-1.7.1.min.js"></script>
 <script type="text/javascript" src="static/js.js"></script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-35178850-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </head>
 <body>
 
