47da7199db9165c0b47c63c1a33cf9f07fa01e7c
Liang Sun <i@liangsun.org>
2013-07-10 19:00:29 +0800

    Update script.

--- changing-author-info.sh
+++ changing-author-info.sh
@@ -7,12 +7,12 @@ am="$GIT_AUTHOR_EMAIL"
 cn="$GIT_COMMITTER_NAME"
 cm="$GIT_COMMITTER_EMAIL"
 
-if [ "$GIT_COMMITTER_EMAIL" = "alazyrabbit@6050238e-8759-11de-82e5-a1837a67dc72" ]
+if [ "$GIT_COMMITTER_EMAIL" = "i@liangsun.org@6050238e-8759-11de-82e5-a1837a67dc72" ]
 then
     cn="Liang Sun"
     cm="i@liangsun.org"
 fi
-if [ "$GIT_AUTHOR_EMAIL" = "alazyrabbit@6050238e-8759-11de-82e5-a1837a67dc72" ]
+if [ "$GIT_AUTHOR_EMAIL" = "i@liangsun.org@6050238e-8759-11de-82e5-a1837a67dc72" ]
 then
     an="Liang Sun"
     am="i@liangsun.org"
